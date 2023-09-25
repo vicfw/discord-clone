70da6b38c2ae6b8d5ed96cb732ac9e5377e2df43
Farid <faridforward222@gmail.com>
2023-09-25 14:56:03 +0330

    changed packege to what he got

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/api/uploadthing", "/api/socket/io"],
+  publicRoutes: ["/", "/api/uploadthing", "/api/socket/io"],
 });
 
 export const config = {
